917d56c22f305d730522e904643ccd48166fc0fb
Biperson Poudel <bipersonpoudel@gmail.com>
2026-03-17 09:45:01 +0545

    Resolved conflicts and finalized app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import os
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
@@ -14,26 +12,6 @@ gemini_model = None
 if GEMINI_API_KEY:
     genai.configure(api_key=GEMINI_API_KEY)
     gemini_model = genai.GenerativeModel("gemini-1.5-flash")
-=======
-=======
-from urllib import response
-
->>>>>>> a70f3f1 (Debugged API issue)
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from google import genai
-import os
-from dotenv import load_dotenv
-from fastapi.middleware.cors import CORSMiddleware
-
-load_dotenv()
-
-api_key = os.getenv("GEMINI_API_KEY")
-if not api_key:
-    raise ValueError("GEMINI_API_KEY missing")
-
-client = genai.Client(api_key=api_key)  # new SDK
->>>>>>> 6efec07 (Merged)
 
 app = FastAPI()
 
@@ -45,7 +23,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-<<<<<<< HEAD
 
 class Message(BaseModel):
     message: str
@@ -74,36 +51,3 @@ def chat(msg: Message):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Gemini API error: {str(e)}")
     return {"message": reply}
-=======
-class ChatRequest(BaseModel):
-    message: str
-@app.post("/chat")
-async def chat(request: ChatRequest):
-    try:
-        response = client.models.generate_content(
-            model="models/gemini-flash-latest",  # your working model
-            contents=request.message
-        )
-        return {"message": response.text}
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-<<<<<<< HEAD
->>>>>>> 6efec07 (Merged)
-=======
-
-
-# @app.post("/chat")
-# async def chat(request: ChatRequest):
-#     try:
-#         response = client.models.generate_content(
-#             model="gemini-1.5-flash",
-#             contents=request.message
-#         )
-#         return {"message": response.text}
-
-#     except Exception as e:
-#         import traceback
-#         traceback.print_exc()
-#         raise HTTPException(status_code=500, detail=str(e))
->>>>>>> a70f3f1 (Debugged API issue)
